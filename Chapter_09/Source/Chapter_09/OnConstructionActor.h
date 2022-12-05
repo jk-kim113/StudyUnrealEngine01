6bc47a2889ae55f03ebd771a0211ef34f1de9f7a
jkkim113 <rlawhdrud113@gmail.com>
2022-12-05 10:09:55 +0900

    Complete Chapter_09

---
+++ Chapter_09/Source/Chapter_09/OnConstructionActor.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "OnConstructionActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAPTER_09_API AOnConstructionActor : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AOnConstructionActor();
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	UPROPERTY(EditAnywhere)
+		bool ShowStaticMesh;
+};
---
+++ Chapter_09/Source/Chapter_09/TileType.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TileType.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class CHAPTER_09_API UTileType : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY()
+		int32 MovementCost;
+
+	UPROPERTY()
+		bool CanBeBuiltOn;
+
+	UPROPERTY()
+		FString TileName;
+};
